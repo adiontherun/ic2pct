0cf45b436a8c830574068159444671c662de85c4
gaurangp22 <thegaurangpant22@gmail.com>
2026-01-05 15:03:48 +0530

    new ones

--- src/components/UniversityMap.tsx
+++ src/components/UniversityMap.tsx
@@ -5,7 +5,7 @@ const UniversityMap = () => {
   return (
     <div className="w-full h-96 bg-gray-200 rounded-lg overflow-hidden shadow-lg">
       <iframe
-        src="https://maps.google.com/maps?q=Galgotias+University,+Greater+Noida,+Uttar+Pradesh&t=&z=15&ie=UTF8&iwloc=&output=embed"
+        src="https://maps.google.com/maps?q=28.4595,77.5324&t=&z=16&ie=UTF8&iwloc=B&output=embed"
         width="100%"
         height="100%"
         style={{ border: 0 }}
